a56a951141007aeeb8fd3cc5e653854b834e8669
Pekka Nikander <pekka.nikander@iki.fi>
2018-08-12 14:36:20 +0300

    Subscription to notifications work

--- Assets/Scripts/BLEPeripheral.cs
+++ Assets/Scripts/BLEPeripheral.cs
@@ -9,11 +9,22 @@ public class BLEPeripheral : MonoBehaviour
     private static extern void BLENativePeripheralSetService(
 	BLENativePeripheral native, string service);
 
-    // XXX Pekka's iPhone on Pekka's Mac
-    public string device = "9504E2CA-6C5D-4951-9D88-62FD75E608B9";
+    [DllImport ("Unity3D_BLE")]
+    private static extern void BLENativePeripheralAddCharacteristic(
+	BLENativePeripheral native, string characteristic);
+
+    [DllImport ("Unity3D_BLE")]
+    private static extern void BLENativePeripheralRemoveCharacteristic(
+	BLENativePeripheral native, string characteristic);
+
+    public string device = "9504E2CA-6C5D-4951-9D88-62FD75E608B9"; // XXX Pekka's iPhone on Pekka's Mac
+
     public string service = "180F"; // Battery
+    protected string _service;
+
     // XXX Should have an array of characteristics
     public string characteristic = "2a19"; // Battery level
+    protected string _characteristic;
 
     public bool connect = false;
     protected bool _connect;
@@ -29,9 +40,8 @@ public class BLEPeripheral : MonoBehaviour
 	BLE.PeripheralDiscovered -= PeripheralDiscovered;
     }
 
-    void Update() {
+    protected void UpdateConnect() {
 	if (connect != _connect) {
-	    // XXX Refactor into a method of its own
 	    if (connect) {
 		if (native == null) {
 		    Log("Peripheral not available, cannot connect");
@@ -51,13 +61,38 @@ public class BLEPeripheral : MonoBehaviour
 	}
     }
 
+    protected void UpdateService() {
+	if (service != _service && native != null) {
+	    BLENativePeripheralSetService(native, service);
+	    _service = service;
+	}
+    }
+
+    protected void UpdateCharacteristics() {
+	if (_characteristic != characteristic && native != null) {
+	    if (_characteristic != null && _characteristic != "") {
+		BLENativePeripheralRemoveCharacteristic(native, _characteristic);
+		_characteristic = null;
+	    }
+	    if (characteristic != null && characteristic != "") {
+		BLENativePeripheralAddCharacteristic(native, characteristic);
+	    }
+	    _characteristic = characteristic;
+	}
+    }
+
+    void Update() {
+	UpdateConnect();
+	UpdateService();
+	UpdateCharacteristics();
+    }
+
     void PeripheralDiscovered(BLENativePeripheral p) {
 	// Log("Discovered " + device);
 	if (device == p.identifier) {
 	    if (native == null) {
 		Log("Adapted " + device);
 		native = p;
-		BLENativePeripheralSetService(native, service);
 	    }
 	}
     }
