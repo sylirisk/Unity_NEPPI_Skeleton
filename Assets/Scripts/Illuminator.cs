0a8109da73c368c4b0751e3ff0f6333de8570f0e
sylirisk <sylirisk@users.noreply.github.com>
2018-09-02 08:17:44 +0300

    Forgot to add this script earlier
    This script handles the appearance of the ball

---
+++ Assets/Scripts/Illuminator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Illuminator : MonoBehaviour {
+
+    Light lightComp;
+
+    private void Start()
+    {
+        lightComp = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update () 
+    {
+
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            lightComp.intensity = 1;
+        } else {
+            lightComp.intensity = 0;
+        }
+	}
+}
